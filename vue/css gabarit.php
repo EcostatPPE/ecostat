560557b0ff655a62c875644d2e6f61b760801b94
KiyozEnc <kevinturmel@gmail.com>
2015-05-21 21:47:18 +0200

    Suppression fichiers crade

--- vue/css gabarit.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="style/general.css" />
-<link rel="stylesheet" href="style/menu.css" />
-<link rel="stylesheet" href="style/body.css" />
-<link rel="stylesheet" href="style/footer.css" />
-<link rel="stylesheet" href="style/aside.css" />
-<link rel="stylesheet" href="style/sondage.css" /> <!-- SONDAGE -->
